1db7b6fe3518b106f1b4594514ed45fc2102f535
Hohman <0308288@students.dadeschools.net>
2019-02-15 10:31:12 -0500

    finished logic of car game

--- main.js
+++ main.js
@@ -1,34 +1,80 @@
-// Variable
-
-let car = {
+// VARIABLES
+const car = {
     make: 'Thanos',
     model: 'Car',
-    color: 'Purple',
+    color: 'purple',
     mileage: 0,
     isWorking: true,
 
     driveToWork(){
         alert(`Old Mileage: ${this.mileage}`);
-        this.mileage = this.mileage + 8;
+        this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
+
+    driveAroundUSA(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 6000;
+        alert(`New Mileage: ${this.mileage}`);
+        aler('Car needs a tuneup!')
+        this.isWorking = false;
+    },
+
+    getTuneUp(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert('Honk! Honk!');
+    }
 }
 
 
-// Functions
+// FUNCTIONS
 
-const rewriteStats(){
-
+const reWriteStats = () =>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`Is Working: ${car.isWorking}`);
+    console.log(`-----------------------------------`);
 }
 
-/* Captures keyboard input. Depending on the letter pressed  it will "call" (execute) different functions. */
+// Logs all of our car's current stats to the console
+/*
+function reWriteStats(){
+
+}
+*/
+
+// MAIN PROCESS
+// Captures keyboard input. Depending on the letter pressed  it will "call" (execute) different functions. 
 
 document.onkeyup = (event) => {
 
     // Captures the key press and converts it to lower case, and saves it to a variable
-    let letter = String.fromCharcode(event.keyCode).toLowerCase();
-
-    if (letter === 'd'){
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    if (letter === "h"){
+        car.honk();
+        reWriteStats();
+    }
+    
+    
+    if (letter === "d"){
         car.driveToWork();
+        reWriteStats();
+    }
+
+    if (letter === "r"){
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t"){
+        car.getTuneUp();
+        reWriteStats();
     }
 }
